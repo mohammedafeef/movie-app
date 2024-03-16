45e5ded35fd2cc1089092caf71db6f6fa12099e3
mohammedafeef <mohammedafeef.maf@gmail.com>
2024-03-16 12:23:19 +0530

    config: setup tailwind and font family

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 function App() {
-	return <div>Hello, world</div>;
+	return <div className="bg-black text-lg text-white">Hello, world</div>;
 }
 
 export default App;
